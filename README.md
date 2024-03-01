99f9dcc71b3a8b2e11da10209757be271ba591f9
MilekJakub <jakub.milek@happyteam.io>
2024-03-01 19:11:51 +0100

    Entry point

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Forgotten-Realm
+
+2D Platformer made in C++ and OpenGL.
+
+To-Do List:
+- Entry Point
+- Game Loop
+- Player Character
+- Simple Level Design
+- Collision Detection
+- Camera
+- Basic UI
+
---
+++ include/Game.h
@@ -0,0 +1,13 @@
+#ifndef FORGOTTEN_REALM_GAME_H
+#define FORGOTTEN_REALM_GAME_H
+
+#endif //FORGOTTEN_REALM_GAME_H
+
+class Game {
+public:
+    Game();
+    ~Game();
+
+    void update();
+    void render();
+};
