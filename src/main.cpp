ff79065d2c879b163fec87bb55fadda2fd9a0856
MilekJakub <jakub.milek@happyteam.io>
2024-03-02 21:11:40 +0100

    add glfw and glew

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,21 @@
+#include <GLFW/glfw3.h>
+
 int main()
 {
+    GLFWwindow* window;
+
+    if (!glfwInit())
+    {
+        return 1;
+    }
+
+    window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    if (! window)
+    {
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+
     return 0;
 }
