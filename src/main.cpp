1b3972aa9801a0442f05742edc66c39692ec2261
MilekJakub <jakub.milek@happyteam.io>
2024-03-03 10:45:10 +0100

    create basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,57 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <cstdio>
 
-int main()
+int main(int argc, char *argv[])
 {
-    GLFWwindow* window;
-
     if (!glfwInit())
     {
         return 1;
     }
 
-    window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
-    if (! window)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Forgotten Realm", nullptr, nullptr);
+    if (!window)
     {
         glfwTerminate();
         return 1;
     }
+
+    int bufferWidth, bufferHeight;
+    glfwGetFramebufferSize(window, &bufferWidth, &bufferHeight);
+
     glfwMakeContextCurrent(window);
 
+    // Allow modern extension features
+    glewExperimental = GL_TRUE;
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        fprintf(stderr, "[ERROR]: GLEW initialisation failed. %s\n", glewGetErrorString(err));
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return 1;
+    }
+
+    fprintf(stdout, "[INFO]: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+
+    glViewport(0, 0, bufferWidth, bufferHeight);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
     return 0;
 }
